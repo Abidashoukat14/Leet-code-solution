0bf26dbc20373a48c7ba2bad117cf2948428f68f
Abidashoukat14 <abidashoukat14@gmail.com>
2024-11-03 09:38:53 -0800

    Time: 544 ms (8.15%), Space: 16.7 MB (97.33%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -0,0 +1,17 @@
+class Solution:
+    def permutations(self,nums:list[int],idx:int,n:int,res:list[list[int]]):
+        if idx==n:
+            if nums[:] in res:
+                return
+            else:        
+                res.append(nums[:])
+                return
+        for i in range(idx,n+1):
+            nums[idx],nums[i]=nums[i],nums[idx]
+            self.permutations(nums,idx+1,n,res)
+            nums[idx],nums[i]=nums[i],nums[idx]
+    def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        res=[]
+        self.permutations(nums,0,len(nums)-1,res)
+        return res
+    
