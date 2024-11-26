7f698e6123f97c81e3f8e924a55dde4441ce6cec
Abidashoukat14 <abidashoukat14@gmail.com>
2024-11-26 07:19:04 -0800

    Time: 0 ms (100%), Space: 18.3 MB (5.23%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,16 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        #create hashmap for storing values with indexs
+        hashmap={}  
+        #start a loop from 0 index to last index for search numbers whose sum equal to target 
+        for i in range(len(nums)):
+            # get second number which we have to search  
+            secNum= target - nums[i] 
+            # check the second number is in hashmap or not if it is then return indexs of both numbers if this sec num present given nums or not 
+            if secNum in hashmap:
+                return [hashmap[secNum],i]
+            # if second number is not in hashmap means if this num is in nums or not if it is return indexes otherwise add it in hashmap     
+            else:
+                hashmap[nums[i]]=i    
     
-        n = len(nums)
-        for i in range(n - 1):
-            for j in range(i + 1, n):
-                if nums[i] + nums[j] == target:
-                    return [i, j]
-        return []  # No solution found    
+    
