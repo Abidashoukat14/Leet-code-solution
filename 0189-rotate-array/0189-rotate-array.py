890efddd9b3b7c54af5087136cac6b26dee92df2
Abidashoukat14 <abidashoukat14@gmail.com>
2025-02-06 07:42:27 -0800

    Time: 8 ms (32.62%), Space: 25.7 MB (40.71%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.py
+++ 0189-rotate-array/0189-rotate-array.py
@@ -3,12 +3,13 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        k %= len(nums)
-        def reverseSolution(left,right):
+        k %=len(nums)
+        def ReverseSolution(left,right):
             while left<right:
-                nums[left],nums[right] =nums[right],nums[left]
+                nums[left],nums[right]=nums[right],nums[left]
                 left+=1
                 right-=1
-        reverseSolution(0,len(nums)-1)
-        reverseSolution(0,k-1)
-        reverseSolution(k,len(nums)-1)        
+        ReverseSolution(0,len(nums)-1)  
+        ReverseSolution(0,k-1)
+        ReverseSolution(k,len(nums)-1)      
+
